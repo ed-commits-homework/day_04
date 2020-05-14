7319c552653f9241cb6824d0dfbcf302e55ba8a7
ed-commits <edward.alexander.cant@gmail.com>
2020-05-14 14:28:34 +0100

    likes to eat

--- friends.rb
+++ friends.rb
@@ -1,3 +1,15 @@
 def get_name(person)
   return person[:name]
 end
+
+def favourite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_to_eat(person, food)
+  if person[:favourites][:snacks].index(food)
+    return true
+  else
+    return false
+  end
+end
